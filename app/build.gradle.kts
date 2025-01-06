463238533dd32399fce2b8e7d357d1c1c3e6754c
JCJMA1994 <201214026@uns.edu.pe>
2025-01-06 11:27:05 -0500

    error compileSdk change 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.systemfailed.calculadora"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.systemfailed.calculadora"
